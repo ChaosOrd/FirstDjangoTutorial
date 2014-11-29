82c7ce50df3640518e98c8064b239bf71ae5425c
Pavel Rybintsev <punkihoy86@gmail.com>
2014-11-29 17:31:12 +0200

    Initial commit for my django tutorial project

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/chaosord/programming/tutorials/django/FirstDjangoTutolrial/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
